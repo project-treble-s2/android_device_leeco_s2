0d35ccd9994d4d8f64a99d65c2aafe998cce6692
Sauhard Pande <sauhardp@codeaurora.org>
2017-05-30 17:49:53 +0530

    s2: camera: QCamera2: For supporting VNDK and Treble rules.
    Issue: In accordance to treble rules libcamera_client.so cannot be
           used or included. For HAL1 needed support for values of
           Cameraparameters for HAL1 QCameraParameters
    
    Solution: Included Cameraparameters.h and .cpp files to this location
              as well.
    
    Change-Id: If04d8a342d1552d2f0ba665b08b312e98bf8b9f5

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -26,6 +26,7 @@ LOCAL_SRC_FILES += \
 
 #HAL 1.0 source
 LOCAL_SRC_FILES += \
+        HAL/CameraParameters.cpp \
         HAL/QCamera2HWI.cpp \
         HAL/QCameraMuxer.cpp \
         HAL/QCameraMem.cpp \
@@ -90,14 +91,16 @@ LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
         hardware/qcom/display-caf/msm8952/libqservice
-LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl
-LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
+LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libui
+LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder libgui
 ifeq ($(TARGET_TS_MAKEUP),true)
 LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
 
 LOCAL_CLANG := true
+
+LOCAL_STATIC_LIBRARIES := android.hardware.camera.common@1.0-helper
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)

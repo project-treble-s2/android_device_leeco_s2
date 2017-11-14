43f8c4df661c5277b72b29acb92e5bf34b409538
Michael Bestas <mkbestas@lineageos.org>
2017-11-14 17:22:37 +0200

    s2: Camera: Cleanup Obsolete LOCAL_PRELINK_MODULE.
    Change-Id: I2b48ea97aef4dcce31f1354c021d409f68d8dc65

--- camera/mm-image-codec/qomx_core/Android.mk
+++ camera/mm-image-codec/qomx_core/Android.mk
@@ -21,8 +21,7 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/../qexif
 LOCAL_SRC_FILES := qomx_core.c
 
 LOCAL_MODULE           := libqomx_core
-LOCAL_VENDOR_MODULE := true
-LOCAL_PRELINK_MODULE   := false
+LOCAL_VENDOR_MODULE    := true
 LOCAL_SHARED_LIBRARIES := libcutils libdl liblog
 
 LOCAL_32_BIT_ONLY := true

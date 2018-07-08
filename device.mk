81123ff23b50ca83415c975f4edb84e56e3f497a
Akash Mondal <a98mondal@gmail.com>
2018-07-08 12:46:14 +0000

    s2: Added h/w keys and backlight stuffs
    Signed-off-by: Akash Mondal <a98mondal@gmail.com>

--- device.mk
+++ device.mk
@@ -24,7 +24,7 @@ $(call inherit-product, vendor/leeco/s2/s2-vendor.mk)
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res

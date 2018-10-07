034a6968a76fc78b05bcb6a6e7de68f9ccf64503
Bruno Martins <bgcngm@gmail.com>
2018-10-08 00:25:18 +0100

    s2: Create root folders outside of vendor init script
     * The core SELinux policies for vendor_init are being restricted as to
       comply with Treble. Simply adding a local rule to allow creating
       folders under rootfs would be enough, but at the same time defining
       the extra root folders and making use of AID/fs configuration file
       to set perms is more neat and clean.
    
     * Clean up config.fs
    
    Change-Id: Idabd7ae72e4c4fd9daac5ba3be3e6236f79f251b
    Signed-off-by: Akash Mondal <a98mondal@gmail.com>

--- config.fs
+++ config.fs
@@ -28,12 +28,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: BLOCK_SUSPEND NET_ADMIN
 
-[system/bin/cnss-daemon]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: NET_BIND_SERVICE
-
 [vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
@@ -41,12 +35,6 @@ group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
 [system/vendor/bin/pm-service]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -149,12 +137,6 @@ group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
 [firmware/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[bt_firmware/]
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM

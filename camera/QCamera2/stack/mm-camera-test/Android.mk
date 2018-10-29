d88b4689c724e9ddc5a711fbdfa54f8aa5bbce51
Bruno Martins <bgcngm@gmail.com>
2018-10-29 12:37:02 +0000

    s2: camera: Use generated kernel headers
    Change-Id: I1508ce54fd99e87f2857e6c6141d5e90f5e9892b

--- camera/QCamera2/stack/mm-camera-test/Android.mk
+++ camera/QCamera2/stack/mm-camera-test/Android.mk
@@ -2,7 +2,6 @@ OLD_LOCAL_PATH := $(LOCAL_PATH)
 LOCAL_PATH:=$(call my-dir)
 
 # Build command line test app: mm-qcamera-app
-include $(LOCAL_PATH)/../../../common.mk
 include $(CLEAR_VARS)
 
 LOCAL_CFLAGS:= \
@@ -37,9 +36,6 @@ LOCAL_C_INCLUDES+= \
         $(LOCAL_PATH)/../common \
         $(LOCAL_PATH)/../../../mm-image-codec/qexif \
         $(LOCAL_PATH)/../../../mm-image-codec/qomx_core
-
-LOCAL_C_INCLUDES+= $(kernel_includes)
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 
 LOCAL_CFLAGS += -DCAMERA_ION_HEAP_ID=ION_IOMMU_HEAP_ID
 ifeq ($(TARGET_BOARD_PLATFORM),msm8974)
@@ -92,6 +88,8 @@ LOCAL_CLANG := true
 LOCAL_MODULE:= mm-qcamera-app
 LOCAL_VENDOR_MODULE := true
 
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
+
 include $(BUILD_EXECUTABLE)
 
 # Build tuning library
@@ -129,9 +127,6 @@ LOCAL_C_INCLUDES+= \
         $(LOCAL_PATH)/../common \
         $(LOCAL_PATH)/../../../mm-image-codec/qexif \
         $(LOCAL_PATH)/../../../mm-image-codec/qomx_core
-
-LOCAL_C_INCLUDES+= $(kernel_includes)
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 
 LOCAL_CFLAGS += -DCAMERA_ION_HEAP_ID=ION_IOMMU_HEAP_ID
 ifeq ($(TARGET_BOARD_PLATFORM),msm8974)
@@ -175,6 +170,8 @@ LOCAL_CFLAGS += -Wall -Wextra -Werror
 
 LOCAL_SHARED_LIBRARIES:= \
          libcutils libdl libmmcamera_interface liblog
+
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 
 LOCAL_MODULE_TAGS := optional
 

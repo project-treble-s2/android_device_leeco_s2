db56a1d8fa94afff334ac54129f5860e19cd42f1
Bruno Martins <bgcngm@gmail.com>
2018-12-30 19:58:56 +0000

    s2: Import XiaomiDoze and convert into LeEcoParts
    Change-Id: I425ad525edcc63c2dcccd1e2fb8459f442744970

--- device.mk
+++ device.mk
@@ -261,6 +261,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/msm_irqbalance_little_big.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_little_big.conf
 
+# LeEco Parts
+PRODUCT_PACKAGES += \
+    LeEcoParts
+
 # Libshims
 PRODUCT_PACKAGES += \
     libshims_camera \

99e9a9298d8d2c2d84716d83f99b79df0c1b2fa5
Pawin Vongmasa <pawin@google.com>
2019-03-12 19:56:00 -0700

    s2: Enable Codec2
    Test: make cts -j123 && cts-tradefed run cts-dev -m \
    CtsMediaTestCases --compatibility:module-arg \
    CtsMediaTestCases:include-annotation:\
    android.platform.test.annotations.RequiresDevice
    
    Bug: 128456533
    Change-Id: Ibe051f4b19c2dbb0a208e751a3b7a332a329b447

--- device.mk
+++ device.mk
@@ -169,6 +169,19 @@ PRODUCT_COPY_FILES +=  \
 PRODUCT_PACKAGES += \
     charger_res_images
 
+# Codec2 modules
+PRODUCT_PACKAGES += \
+    com.android.media.swcodec \
+    libsfplugin_ccodec
+
+# Codec2 files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
+
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
     libcnefeatureconfig

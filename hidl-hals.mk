0b89af47281dee9ed72f5267c7d340ca67f2bc9c
dudkin-2005 <dudkin-2005@yandex.ru>
2019-04-13 21:47:40 +0300

    go treble

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -37,7 +37,7 @@ PRODUCT_PACKAGES += \
 
 # Configstore
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.1-service
+    android.hardware.configstore@1.0-service
 
 # Consumer IR
 PRODUCT_PACKAGES += \

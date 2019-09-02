c1644bd40afbacd630274293f9ad0ec0bc99acb3
Kuba Wojciechowski <nullbytepl@gmail.com>
2019-09-02 21:44:31 +0200

    s2: shims: goodix: kill me right now

--- libshims/goodix/goodix.cpp
+++ libshims/goodix/goodix.cpp
@@ -3,6 +3,8 @@
 #include <utils/StrongPointer.h>
 
 namespace android {
+    extern android::sp<android::IInterface> _ZN7android8security8keystore16IKeystoreService11asInterfaceERKNS_2spINS_7IBinderEEE(const android::sp<android::IBinder>& obj);
+	
     android::sp<android::IInterface> _ZN7android8security16IKeystoreService11asInterfaceERKNS_2spINS_7IBinderEEE(const android::sp<android::IBinder>& obj) {
         return _ZN7android8security8keystore16IKeystoreService11asInterfaceERKNS_2spINS_7IBinderEEE(obj);
     }

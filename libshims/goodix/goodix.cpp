578543b70cc34b606cf84095b585521c0a980518
Kuba Wojciechowski <nullbytepl@gmail.com>
2019-09-02 21:34:20 +0200

    s2: shims: goodix: add missing include

--- libshims/goodix/goodix.cpp
+++ libshims/goodix/goodix.cpp
@@ -1,5 +1,6 @@
 #include <binder/IInterface.h>
 #include <binder/IBinder.h>
+#include <utils/StrongPointer.h>
 
 namespace android {
     extern "C" android::sp<android::IInterface> _ZN7android8security16IKeystoreService11asInterfaceERKNS_2spINS_7IBinderEEE(const android::sp<android::IBinder>& obj) {

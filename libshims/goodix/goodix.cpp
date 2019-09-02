43a158dec2d4d4ad0ff3c7a2c277ca56e872cb3a
Kuba Wojciechowski <nullbytepl@gmail.com>
2019-09-02 21:59:22 +0200

    s2: shims: goodix: damn namespace

--- libshims/goodix/goodix.cpp
+++ libshims/goodix/goodix.cpp
@@ -2,10 +2,8 @@
 #include <binder/IBinder.h>
 #include <utils/StrongPointer.h>
 
-namespace android {
-    extern android::sp<android::IInterface> _ZN7android8security8keystore16IKeystoreService11asInterfaceERKNS_2spINS_7IBinderEEE(const android::sp<android::IBinder>& obj);
-	
-    android::sp<android::IInterface> _ZN7android8security16IKeystoreService11asInterfaceERKNS_2spINS_7IBinderEEE(const android::sp<android::IBinder>& obj) {
-        return _ZN7android8security8keystore16IKeystoreService11asInterfaceERKNS_2spINS_7IBinderEEE(obj);
-    }
+extern android::sp<android::IInterface> _ZN7android8security8keystore16IKeystoreService11asInterfaceERKNS_2spINS_7IBinderEEE(const android::sp<android::IBinder>& obj);
+
+android::sp<android::IInterface> _ZN7android8security16IKeystoreService11asInterfaceERKNS_2spINS_7IBinderEEE(const android::sp<android::IBinder>& obj) {
+    return _ZN7android8security8keystore16IKeystoreService11asInterfaceERKNS_2spINS_7IBinderEEE(obj);
 }

7234f32211613f10faaa89948ff1a1cbaef2629d
volfram <45515661+dudkin-2005@users.noreply.github.com>
2019-12-08 22:27:48 +0300

    Remove LKMD

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -261,19 +261,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.min.fling_velocity=160 \
 ro.max.fling_velocity=20000
 
-# Userspace LMKD
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.lmk.low=1001 \
-ro.lmk.medium=800 \
-ro.lmk.critical=0 \
-ro.lmk.critical_upgrade=false \
-ro.lmk.upgrade_pressure=70 \
-ro.lmk.downgrade_pressure=90 \
-ro.lmk.kill_heaviest_task=true \
-ro.lmk.kill_timeout_ms=100 \
-ro.lmk.use_minfree_levels=false \
-ro.lmk.debug=true
-
 # GCAM Fixes
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.camera.eis.enable=1 \
